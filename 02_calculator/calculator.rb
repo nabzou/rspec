05021da9eb91666ba16fe42478e850e4cadffea5
nabzou <nabila.blidi@hotmail.fr>
2018-04-09 18:52:22 +0200

    travail sur rspec

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,21 @@
 #write your code here
+def add(premier_parametre, deuxieme_parametre)
+  premier_parametre + deuxieme_parametre
+end
+def subtract(premier_parametre, deuxieme_parametre)
+  premier_parametre - deuxieme_parametre
+#on va appliquer plus ou moins la même methode pour les multiplications ou les divisions
+end
+
+def sum(chiffres)
+	 total = 0
+	 chiffres.each { |chiffre| total += chiffre }
+	 total
+#.each= récupérer les sommes une à une (iteration) 
+end
+
+def multiply(premier_parametre, *autres)
+  total = premier_parametre
+  autres.each { |chiffre| total *= chiffre }
+  total
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,11 @@
 #write your code here
+def echo(string)
+  string
+end
+def shout(string)
+  string.upcase
+end
+
+def repeat
+	Array.new(2, string).join(" ")
+end
